c4320614e674f8acc92d9b685749a22dc536e5c1
orodrigojose <contato.rodrigosilv@gmail.com>
2024-12-24 18:20:10 -0300

    feat(classroom 2) basic types string

---
+++ tipos_basicos/string.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var hello string = "Olá, mundo!"
+	var question string = "Como vai?"
+
+	// Concatenação
+	meet := hello + question
+
+	fmt.Println(meet)
+	fmt.Println(strings.ToUpper(meet))
+	fmt.Println(strings.Contains(meet, "mundo"))
+}
