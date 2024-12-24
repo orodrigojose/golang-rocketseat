808865c375fc52854b0b398a5bbb3a2c091f76c7
orodrigojose <contato.rodrigosilv@gmail.com>
2024-12-24 17:54:19 -0300

    feat(classroom 2) basic types float

--- tipos_basicos/main.go
+++ tipos_basicos/main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var idade int = 30
+	//     INT
+	// var idade int = 30
 	// var contador int32 = 2
 	// var indice int8 = 1
 
-	fmt.Println("Idade: ", idade)
+	// fmt.Println("Idade: ", idade)
+
+	// FLOAT
+	// var floatNumber float32 = 1.1
+	var pi float64 = 3.14
+	var raio float64 = 2.5
+	area := pi * raio * raio
+
+	fmt.Println("Area do circulo é ", area)
 }
